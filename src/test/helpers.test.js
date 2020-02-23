245ba71dbc6ad8eeaa5127dae14e1ac7efae3b25
mehdibenyebka <mehdib030@gmail.com>
2020-02-23 15:07:49 -0600

    Adding tests

--- src/test/helpers.test.js
+++ src/test/helpers.test.js
@@ -1,5 +1,13 @@
 const sum = require('../utils/utils');
-//TODO:Remove and add real unit tests
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+beforeEach(() => {
+  
+});
+
+afterEach(() => {
+  
 });
